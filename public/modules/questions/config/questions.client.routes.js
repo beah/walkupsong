65f09fc587a088ef60ce4b1c5b1080ecaea48d64
beah <beah.burger@gmail.com>
2015-11-22 18:39:09 -0500

    tried to get create and list fully working but failed

--- public/modules/questions/config/questions.client.routes.js
+++ public/modules/questions/config/questions.client.routes.js
@@ -5,6 +5,10 @@ angular.module('questions').config(['$stateProvider',
 	function($stateProvider) {
 		// Questions state routing
 		$stateProvider.
+		state('create-question', {
+			url: '/questions/create',
+			templateUrl: 'modules/questions/views/create-question.client.view.html'
+		}).
 		state('questions', {
 			url: '/questions',
 			templateUrl: 'modules/questions/views/questions.client.view.html'
